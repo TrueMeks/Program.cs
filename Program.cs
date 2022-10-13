cf06587e1df7fa02a4bedd1f673ff959a7b867d7
TrueMaks <makslange@yandex.ru>
2022-10-13 21:37:20 +0300

    Работа с задачами

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+int a = 31;
+double b = -12.45;
+string c = "world";
+char d = '@';
+bool e = true;
+Console.WriteLine("Hello world");
+*/
+/* задача 1
+
+Console.Write("Input first namber: ");
+int n1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Input second namber: ");
+int n2 = Convert.ToInt32 (Console.ReadLine());
+
+int quad2 = n2 * n1;
+if(quad2 == n1)
+{
+    Console.WriteLine($"Numbers {n1} is a square of {n2}");
+}
+else
+{
+    Console.WriteLine($"Numbers {n1} is not square of {n2}");
+}
+*/
+/*
+
+Задача №2
+
+Console.Write("Input a positive integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = -number;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current ++;
+};
+*/
+// задача № 3
+/*
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number % 10;
+Console.WriteLine($"Last digit of {number} is {result}");
+*/
+
+
